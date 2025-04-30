99196e270105275a8162bd3b3edc5f3ac35665da
backas36 <backas36@gmail.com>
2025-04-30 23:55:22 +0800

    chore(ui): install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-    return <div>Hello, World!</div>;
+    return (
+        <div className='flex h-screen flex-col items-center justify-center'>
+            <h1 className='text-4xl font-bold'>Hello, World!</h1>
+        </div>
+    );
 }
 
 export default App;
