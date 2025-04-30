c490ed7386ce88a205311a30f57716927fe20368
backas36 <backas36@gmail.com>
2025-04-30 23:41:10 +0800

    chore(cz-config): setup cz-config

---
+++ .cz-config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+    types: [
+        { value: "feat", name: "feat:     🎉  A new feature" },
+        { value: "fix", name: "fix:      🐛  A bug fix" },
+        { value: "docs", name: "docs:     📎  Documentation only changes" },
+        { value: "style", name: "style:    💈  Changes that do not affect the meaning of the code" },
+        { value: "refactor", name: "refactor: 🔄  A code change that neither fixes a bug nor adds a feature" },
+        { value: "perf", name: "perf:     🤹  A code change that improves performance" },
+        { value: "test", name: "test:     ⌛ Adding missing tests or correcting existing tests" },
+        { value: "build", name: "build:    🛠️  Changes that affect the build system or external dependencies" },
+        { value: "ci", name: "ci:       👷  Changes to CI configuration files and scripts" },
+        { value: "chore", name: "chore:    ⚙️ Other changes that don't modify src or test files" },
+        { value: "revert", name: "revert:   ⏪️  Reverts a previous commit" },
+    ],
+    scopes: [{ name: "core" }, { name: "ui" }, { name: "api" }, { name: "docs" }, { name: "test" }],
+    allowCustomScopes: true,
+    allowBreakingChanges: ["feat", "fix"],
+    messages: {
+        type: "Select the type of change that you're committing:",
+        scope: "Select a scope (optional):",
+        customScope: "Denote the scope of this change:",
+        subject: "Write a short, imperative tense description of the change:\n",
+        body: 'Provide a longer description of the change (optional). Use "|" to break new line:\n',
+        breaking: "List any breaking changes (optional):\n",
+        footer: "List any issues closed by this change (optional). E.g.: #31, #34:\n",
+    },
+    subjectLimit: 50,
+};
