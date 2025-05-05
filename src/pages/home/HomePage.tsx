c8a7487eaf934b99c229b636314c215826349a2d
backas36 <backas36@gmail.com>
2025-05-06 00:09:17 +0800

    chore: add custom color in shadcn 🎨

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,6 +3,10 @@ const HomePage = () => {
     return (
         <div className='flex h-screen flex-col items-center justify-center'>
             <img src={floor01} alt='floor01' />
+            {/*<img src={floor01} alt='floor01' />*/}
+            <p className='text-custom-color'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti, maxime!
+            </p>
         </div>
     );
 };
